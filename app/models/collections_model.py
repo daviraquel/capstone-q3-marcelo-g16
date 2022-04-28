6870c349db082c9a6db49e75c36fc0b279e4c833
Doug <doug.aluz@gmail.com>
2022-04-28 10:54:48 -0300

    fix: changing name of column collection to unique and description to nullable

--- app/models/collections_model.py
+++ app/models/collections_model.py
@@ -18,7 +18,7 @@ class CollectionsModel(db.Model):
 
     __tablename__ = "collections" 
     id = Column(Integer, primary_key=True)
-    name = Column(String(120), nullable=False)
+    name = Column(String(120), nullable=False, unique=True)
     description = Column(String(250), nullable=False)
 
     #adicionar relacionamento
